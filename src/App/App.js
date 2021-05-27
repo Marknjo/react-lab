6c8cefa7064773febb2545d47ec01df0d4395c6b
Marknjo <marknjo7@gmail.com>
2021-05-27 16:06:51 +0300

    Add expense tracker styles

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,13 @@
 import "./styles/main.scss";
 import { consoleSeparator } from "./helpers/consoleSeparator";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   consoleSeparator();
   return (
     <div>
       <h1>Hello React App</h1>
-      <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/App/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurace</h2>
+        <div>1294.67</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
