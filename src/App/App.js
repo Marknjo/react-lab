8193a321e802fe9e5287bd0c8219346e83e90b06
Marknjo <marknjo7@gmail.com>
2021-05-27 14:28:45 +0300

    Initial Commit

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import "./styles/main.scss";
+import { consoleSeparator } from "./helpers/consoleSeparator";
+
+function App() {
+  return (
+    <div>
+      <p>Hello React App</p>
+
+      {consoleSeparator()}
+    </div>
+  );
+}
+
+export default App;
