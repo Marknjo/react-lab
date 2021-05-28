5f18402b1fec420868427d6cb6afe611ef8ed872
Marknjo <marknjo7@gmail.com>
2021-05-28 14:11:51 +0300

    Implement chart componets logic

--- src/App/components/Chart/Chart.js
+++ src/App/components/Chart/Chart.js
@@ -1,5 +1,21 @@
 import './Chart.css';
+import ChartBar from './ChartBar';
 
-const Chart = function () {};
+const Chart = function ({ dataPoints }) {
+  return (
+    <div className="chart">
+      {dataPoints.map(dataPoint => {
+        return (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={null}
+            label={dataPoint.label}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Chart;
