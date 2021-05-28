f66fe2d413a22083e3784d4aecca3f3c6d22a072
Marknjo <marknjo7@gmail.com>
2021-05-28 13:05:03 +0300

    Implementing expenses filter logic

--- src/App/components/Expenses/Expenses.js
+++ src/App/components/Expenses/Expenses.js
@@ -11,6 +11,25 @@ const Expenses = function ({ items }) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = items.filter(
+    expense => expense.date.getFullYear() === +filteredYear
+  );
+
+  let expensesContent = <p>No expenses found</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(expense => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    });
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -19,16 +38,7 @@ const Expenses = function ({ items }) {
           onChangeFilter={filterChangeHandler}
         />
 
-        {items.map(expense => {
-          return (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          );
-        })}
+        {expensesContent}
       </Card>
     </div>
   );
