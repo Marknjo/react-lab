f997fb36a3353e5c501c7110fb369fa15b6725e5
Marknjo <marknjo7@gmail.com>
2021-05-29 13:33:52 +0300

    Practicals: Defining AddUserInfoForm component

--- src/App/components/AddInfo/AddUserInfoForm.js
+++ src/App/components/AddInfo/AddUserInfoForm.js
@@ -1,10 +1,20 @@
+import Card from '../../UI/Card';
 import styles from './AddUserInfoForm.module.css';
 
 const AddUserInfoForm = function () {
   return (
-    <div>
-      <h2>Form content goes here</h2>
-    </div>
+    <Card>
+      <form className={styles['form-control']}>
+        <div className="form-control__group">
+          <label htmlFor="username">Username</label>
+          <input name="username" type="text" id="username" />
+        </div>
+        <div className="form-control__group">
+          <label htmlFor="age">Username</label>
+          <input name="age" type="number" id="age" />
+        </div>
+      </form>
+    </Card>
   );
 };
 
