3a23febf140881c534917726a22dc4314fd1e7a9
Marknjo <marknjo7@gmail.com>
2021-05-31 12:39:38 +0300

    Advanced: implement useEffect by fetching data on localStorage

--- src/App/App.js
+++ src/App/App.js
@@ -7,14 +7,22 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const checkIsLoggedInStatus = localStorage.getItem('isLoggedIn');
+    if (checkIsLoggedInStatus === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
