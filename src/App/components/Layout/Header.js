231ac8c9679136ab6849cc1901e1f39e37aab48a
Marknjo <marknjo7@gmail.com>
2021-06-01 08:45:12 +0300

    Meals App: Add cart button structure

--- src/App/components/Layout/Header.js
+++ src/App/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import styles from './Header.module.css';
 import mealsImg from '../../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
-const Header = props => {
+const Header = function (props) {
   return (
     <>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={styles['main-image']}>
         <img src={mealsImg} alt="A table full of delicious food" />
