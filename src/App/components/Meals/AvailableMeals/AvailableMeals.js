efc906531e6eca03293184c81bbfe03faf8dfdea
Marknjo <marknjo7@gmail.com>
2021-06-01 08:55:50 +0300

    Meals App: Structure the AvailableMeals component

--- src/App/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/App/components/Meals/AvailableMeals/AvailableMeals.js
@@ -1,1 +1,14 @@
+import { DUMMY_MEALS } from '../../../data/dummy-meals';
+import styles from './AvailableMeals.module.css';
 
+const AvailableMeals = function () {
+  const mealsList = DUMMY_MEALS.map(meal => <li key={meal.id}>{meal.name}</li>);
+
+  return (
+    <section className={styles.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+};
+
+export default AvailableMeals;
