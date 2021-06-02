5c8a46c5218b260e3ae213aed5c373e1ba97fe5c
Marknjo <marknjo7@gmail.com>
2021-06-02 14:54:24 +0300

    Meals App: Animate Your Cart button on add or remove item

--- src/App/components/Layout/HeaderCartButton.js
+++ src/App/components/Layout/HeaderCartButton.js
@@ -1,17 +1,40 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = function ({ onClick }) {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
-  const numberOfCartItem = cartCtx.items.reduce(
+
+  const { items } = cartCtx;
+
+  const numberOfCartItem = items.reduce(
     (curNum, item) => curNum + item.amount,
     0
   );
 
+  const btnStyles = `${styles.button}  ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnIsHighlighted(true);
+
+    const removeTimer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(removeTimer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={btnStyles} onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
