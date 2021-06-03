055182a84947b459bf34724ad344b7e98ecd05d8
Marknjo <marknjo7@gmail.com>
2021-06-03 13:26:44 +0300

    Custom Hooks: Trivial overview to custom hooks

---
+++ src/App/hooks/use-counter.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+const useCounter = function (forwards = true) {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(prevCounter => {
+        if (forwards) {
+          return prevCounter + 1;
+        }
+
+        return prevCounter - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
+};
+
+export default useCounter;
