f02672e4d738a53b1c24a89958dd190fd2f8b1e2
Marknjo <marknjo7@gmail.com>
2021-06-03 11:14:29 +0300

    Http: Loading movies asynchronous logic

--- src/App/App.js
+++ src/App/App.js
@@ -1,33 +1,57 @@
 //import { consoleSeparator } from './helpers/consoleSeparator';
 
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
+/* const dummyMovies = [
+  {
+    id: 1,
+    title: 'Some Dummy Movie',
+    openingText: 'This is the opening text of the movie',
+    releaseDate: '2021-05-18',
+  },
+  {
+    id: 2,
+    title: 'Some Dummy Movie 2',
+    openingText: 'This is the second opening text of the movie',
+    releaseDate: '2021-05-19',
+  },
+]; */
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovieHandler = async function () {
+    try {
+      const filmsResp = await fetch('https://swapi.dev/api/films/');
+
+      let { results } = await filmsResp.json();
+
+      results = results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </Fragment>
   );
