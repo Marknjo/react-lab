431a289ccdf32ba0e9e5747447b33d1bd5e6dc32
Marknjo <marknjo7@gmail.com>
2021-06-03 13:43:36 +0300

    Custom Hooks: Practical example using Firebase to save and fetch data

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,10 @@
 //import { consoleSeparator } from './helpers/consoleSeparator';
 
 import React from 'react';
+import Firebase from './Firebase/Firebase';
 
 function App() {
-  return (
-    <React.Fragment>
-      <div> New Project </div>
-    </React.Fragment>
-  );
+  return <Firebase />;
 }
 
 export default App;
---
+++ src/App/Firebase/README.md
@@ -0,0 +1,11 @@
+# :rocket: A Practical assignement
+
+Add Note Here
+
+## :balloon: Skills Gained in the Session
+
+- Note Skills
+
+## :bookmark_tabs: Footnotes
+
+- Note challenges and things worth noting
