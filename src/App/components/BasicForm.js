8c75db5473e49440d8de6101a5d76ca86be1d6cc
Marknjo <marknjo7@gmail.com>
2021-06-04 12:04:59 +0300

    Forms: Practicals - implementing last name validation

--- src/App/components/BasicForm.js
+++ src/App/components/BasicForm.js
@@ -14,12 +14,21 @@ const BasicForm = props => {
   } = useInput(value => value.trim() !== '' && value.trim().length > 3);
 
   //2. Lastname input logic
+  const {
+    value: enteredLastnameValue,
+    isValid: enteredLastnameIsValid,
+    hasError: lastnameInputHasErrors,
+    inputBlurHandler: lastnameBlurHandler,
+    valueChangeHandler: lastnameChangeHandler,
+    reset: resetLastnameInput,
+    defaultOrErrorStyles: lastnameFieldStyles,
+  } = useInput(value => value.trim() !== '' && value.trim().length > 3);
 
   //3. Email Address input logic
 
   //Form validity
   let formIsValid = false;
-  if (firstnameInputHasErrors) {
+  if (firstnameInputHasErrors && lastnameInputHasErrors) {
     formIsValid = true;
   }
 
@@ -29,38 +38,60 @@ const BasicForm = props => {
     event.preventDefault();
 
     //2.1. Validate First name input (Last name and first name have a similar way of validation)
-    if (!enteredFirstnameIsValid) {
+    //2.2. Validate Last name input
+    if (
+      (!enteredFirstnameIsValid && enteredFirstnameValue < 4) ||
+      (!enteredLastnameIsValid && enteredLastnameValue < 4)
+    ) {
       return;
     }
-    //2.2. Validate Last name input
+
     //2.3. validate email input
 
     //3. Submit data after all validations have passed (For nor console log)
     console.log(enteredFirstnameValue);
+    console.log(enteredLastnameValue);
 
     //4. Reset form fields
     resetFirstnameInput();
+    resetLastnameInput();
   };
 
   return (
     <form onSubmit={submitFormHandler}>
       <div className="control-group">
         <div className={firstnameFieldStyles()}>
-          <label htmlFor="name">First Name</label>
+          <label htmlFor="firstname">First Name</label>
+
           <input
             type="text"
-            id="name"
+            id="firstname"
+            name="firstname"
             onBlur={firstnameBlurHandler}
             onChange={firstnameChangeHandler}
             value={enteredFirstnameValue}
           />
-          {!enteredFirstnameIsValid && (
+
+          {firstnameInputHasErrors && (
             <p className="error-text">First Name field is invalid.</p>
           )}
         </div>
-        <div className="form-control">
-          <label htmlFor="name">Last Name</label>
-          <input type="text" id="name" />
+
+        <div className={lastnameFieldStyles()}>
+          <label htmlFor="lastname">Last Name</label>
+
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            onBlur={lastnameBlurHandler}
+            onChange={lastnameChangeHandler}
+            value={enteredLastnameValue}
+          />
+
+          {lastnameInputHasErrors && (
+            <p className="error-text">Last name field is invalid.</p>
+          )}
         </div>
       </div>
       <div className="form-control">
