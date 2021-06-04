bee7b6a2be42ee292640cdaf1559fd4c8ed731bb
Marknjo <marknjo7@gmail.com>
2021-06-04 12:04:37 +0300

    Forms: Practicals - implementing first name validation

--- src/App/components/BasicForm.js
+++ src/App/components/BasicForm.js
@@ -1,22 +1,74 @@
-const BasicForm = (props) => {
+import useInput from '../hook/use-input';
+
+const BasicForm = props => {
+  //Handle form inputs using the useInput logic
+  //1. First name inputs Logic
+  const {
+    value: enteredFirstnameValue,
+    isValid: enteredFirstnameIsValid,
+    hasError: firstnameInputHasErrors,
+    inputBlurHandler: firstnameBlurHandler,
+    valueChangeHandler: firstnameChangeHandler,
+    reset: resetFirstnameInput,
+    defaultOrErrorStyles: firstnameFieldStyles,
+  } = useInput(value => value.trim() !== '' && value.trim().length > 3);
+
+  //2. Lastname input logic
+
+  //3. Email Address input logic
+
+  //Form validity
+  let formIsValid = false;
+  if (firstnameInputHasErrors) {
+    formIsValid = true;
+  }
+
+  //Form submittion the logic
+  const submitFormHandler = event => {
+    //1.prevent form submission
+    event.preventDefault();
+
+    //2.1. Validate First name input (Last name and first name have a similar way of validation)
+    if (!enteredFirstnameIsValid) {
+      return;
+    }
+    //2.2. Validate Last name input
+    //2.3. validate email input
+
+    //3. Submit data after all validations have passed (For nor console log)
+    console.log(enteredFirstnameValue);
+
+    //4. Reset form fields
+    resetFirstnameInput();
+  };
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={submitFormHandler}>
+      <div className="control-group">
+        <div className={firstnameFieldStyles()}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            onBlur={firstnameBlurHandler}
+            onChange={firstnameChangeHandler}
+            value={enteredFirstnameValue}
+          />
+          {!enteredFirstnameIsValid && (
+            <p className="error-text">First Name field is invalid.</p>
+          )}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+        <div className="form-control">
+          <label htmlFor="name">Last Name</label>
+          <input type="text" id="name" />
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+      <div className="form-control">
+        <label htmlFor="name">E-Mail Address</label>
+        <input type="text" id="name" />
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+      <div className="form-actions">
+        <button disabled={formIsValid}>Submit</button>
       </div>
     </form>
   );
