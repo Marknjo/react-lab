e52b18f917a59ffe72a231dd72d88677d0de3434
Marknjo <marknjo7@gmail.com>
2021-06-05 10:13:00 +0300

    Redux: adding auth, login, userprofile and header components

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,18 @@
 //import { consoleSeparator } from './helpers/consoleSeparator';
 import Counter from './components/Counter';
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import Auth from './components/Auth';
 
 const App = function () {
-  return <Counter />;
+  return (
+    <>
+      <Header />
+      <Auth />
+      <UserProfile />
+      <Counter />
+    </>
+  );
 };
 
 export default App;
