c6969a4b8e8fb9e2df1d54277ada9a3709fd875c
Marknjo <marknjo7@gmail.com>
2021-06-05 12:33:37 +0300

    Redux: Refactor redux index to contain auth and counter slices in separate files (Declutter)

--- src/App/components/Auth.js
+++ src/App/components/Auth.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import useInput from '../Hooks/use-input';
-import { authActions } from '../store';
+import { authActions } from '../store/auth';
 
 import classes from './Auth.module.css';
 
