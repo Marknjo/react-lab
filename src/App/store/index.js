4d66a7204ea1957def7358b71248536981fea5a1
Marknjo <marknjo7@gmail.com>
2021-06-05 10:05:13 +0300

    Redux: Refactoring store to use @latest reduxjs toolkit

--- src/App/store/index.js
+++ src/App/store/index.js
@@ -1,45 +1,37 @@
-import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
   showCounter: true,
 };
 
-//1. create reducer
-const counterReducer = function (state = initialState, action) {
-  if (action.type === 'increment') {
-    return {
-      ...state,
-      counter: state.counter + 1,
-    };
-  }
+//1. createSlice
 
-  if (action.type === 'increase') {
-    return {
-      ...state,
-      counter: state.counter + action.amount,
-    };
-  }
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  if (action.type === 'decrement') {
-    return {
-      ...state,
-      counter: state.counter - 1,
-    };
-  }
+//2. configure store
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 
-  if (action.type === 'toggle') {
-    return {
-      ...state,
-      showCounter: !state.showCounter,
-    };
-  }
-
-  return state;
-};
-
-//2. create store
-const store = createStore(counterReducer);
+export const counterActions = counterSlice.actions;
 
 export default store;
 
