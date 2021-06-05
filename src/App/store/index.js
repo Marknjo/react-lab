d731c3e7a66a3b390d293fe334cd7995f6ac0717
Marknjo <marknjo7@gmail.com>
2021-06-05 07:11:13 +0300

    Redux: Managing multiple types of state values

--- src/App/store/index.js
+++ src/App/store/index.js
@@ -1,22 +1,37 @@
 import { createStore } from 'redux';
 
+const initialState = {
+  counter: 0,
+  showCounter: true,
+};
+
 //1. create reducer
-const counterReducer = function (state = { counter: 0 }, action) {
+const counterReducer = function (state = initialState, action) {
   if (action.type === 'increment') {
     return {
+      ...state,
       counter: state.counter + 1,
     };
   }
 
   if (action.type === 'increase') {
     return {
+      ...state,
       counter: state.counter + action.amount,
     };
   }
 
   if (action.type === 'decrement') {
     return {
+      ...state,
       counter: state.counter - 1,
+    };
+  }
+
+  if (action.type === 'toggle') {
+    return {
+      ...state,
+      showCounter: !state.showCounter,
     };
   }
 
