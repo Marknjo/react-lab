859b8aaf4ccfaa359c268ed3674747193a0ffe7e
Marknjo <marknjo7@gmail.com>
2021-06-05 06:25:45 +0300

    Redux: React redux store configuration

---
+++ src/App/store/index.js
@@ -0,0 +1,36 @@
+import { createStore } from 'redux';
+
+//1. create reducer
+const counterReducer = function (state = { counter: 0 }, action) {
+  if (action.type === 'increament') {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+//2. create store
+const store = createStore(counterReducer);
+
+export default store;
+
+/* //3. Subscribe to the store
+const counterSubscriber = function () {
+  const latestCounter = store.getState();
+
+  console.log(latestCounter);
+};
+
+store.subscriber(counterSubscriber);
+
+//4. dispatch actions
+store.dispatch({ counter: 'increment' });
+store.dispatch({ counter: 'decrement' }); */
