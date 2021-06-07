d7177b58758b951f2d1bbd7fe0aaec6880d2dbd2
Marknjo <marknjo7@gmail.com>
2021-06-07 16:11:40 +0300

    Add support for custom css styles for controlled input element

--- src/App/components/UI/Input/ControlledInput.js
+++ src/App/components/UI/Input/ControlledInput.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './ControlledInput.module.css';
 
-const Input = function ({ label, input }) {
+const Input = function ({ label, input, className }) {
   return (
-    <div className={styles.input}>
+    <div className={`${styles.input} ${className ? className : ''}`}>
       <label htmlFor={input.id}>{label}</label>
       <input {...input} />
     </div>
