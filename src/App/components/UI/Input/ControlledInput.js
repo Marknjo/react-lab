112a0ea858a82bfc325df3510e0007fd2b6f7dea
Marknjo <marknjo7@gmail.com>
2021-06-07 16:38:01 +0300

    Styles for controled input component

--- src/App/components/UI/Input/ControlledInput.js
+++ src/App/components/UI/Input/ControlledInput.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './ControlledInput.module.css';
 
-const Input = function ({ label, input, className }) {
+const Input = function ({ label, input, className, required }) {
   return (
     <div className={`${styles.input} ${className ? className : ''}`}>
-      <label htmlFor={input.id}>{label}</label>
+      <label htmlFor={input.id}>
+        <span>{label}</span>
+
+        {required && <span className={styles['input__required']}>*</span>}
+      </label>
       <input {...input} />
     </div>
   );
