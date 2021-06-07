8bef422411387849d546ddc7b7e46351eb392a8b
Marknjo <marknjo7@gmail.com>
2021-06-07 16:41:27 +0300

    Handling events in the controlled input element

--- src/App/components/UI/Input/ControlledInput.js
+++ src/App/components/UI/Input/ControlledInput.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import styles from './ControlledInput.module.css';
 
-const Input = function ({ label, input, className, required }) {
+const Input = function ({
+  label,
+  input,
+  className,
+  required,
+  onBlur,
+  onChange,
+}) {
+  const blurHandler = () => {
+    onBlur && onBlur();
+  };
+
+  const changeHandler = value => {
+    onChange && onChange(value);
+  };
+
   return (
     <div className={`${styles.input} ${className ? className : ''}`}>
       <label htmlFor={input.id}>
@@ -9,7 +24,7 @@ const Input = function ({ label, input, className, required }) {
 
         {required && <span className={styles['input__required']}>*</span>}
       </label>
-      <input {...input} />
+      <input {...input} onBlur={blurHandler} onChange={changeHandler} />
     </div>
   );
 };
