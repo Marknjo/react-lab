5e978b455dba126502c3f27382c498849f17e6dd
Marknjo <marknjo7@gmail.com>
2021-06-09 09:39:17 +0300

    Adv Redux: add mutiple items to the products using DUMMY_DATA

--- src/App/components/Shop/Products.js
+++ src/App/components/Shop/Products.js
@@ -1,17 +1,36 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_DATA = [
+  {
+    id: 'p1',
+    title: 'Nike Shoe',
+    description: 'Sports and outdoor favourite shoe',
+    price: 125,
+  },
+  {
+    id: 'p2',
+    title: 'Merino runners men',
+    description:
+      'Our men’s merino runners are designed for maximum comfort, grip and performance.',
+    price: 129.95,
+  },
+];
+
 const Products = props => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-          id="#titm01"
-        />
+        {DUMMY_DATA.map(item => (
+          <ProductItem
+            title={item.title}
+            price={item.price}
+            description={item.description}
+            id={item.id}
+            key={item.key}
+          />
+        ))}
       </ul>
     </section>
   );
