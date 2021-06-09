945be0ce91c600b33787883fe373b756d0c7bff4
Marknjo <marknjo7@gmail.com>
2021-06-09 12:43:25 +0300

    Adv Redux: Sync cart with the firebase database

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,49 @@
 //import { consoleSeparator } from './helpers/consoleSeparator';
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import FIREBASE_URL from './configs/firebase';
 
 function App() {
   const isCartHidden = useSelector(state => {
     return state.ui.isCartHidden;
   });
+
+  const cart = useSelector(state => state.cart);
+  const cartUserID = 'cart-mark7692';
+
+  useEffect(() => {
+    const sendData = async function () {
+      try {
+        //1.update data to the database
+        //data format cart-userID.json -PUT format
+        const response = await fetch(`${FIREBASE_URL}${cartUserID}.json`, {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        //check for errors
+        if (!response.ok) {
+          throw new Error('Could update the cart!');
+        }
+
+        //response successful
+
+        throw new Error('Cart was successfully updated');
+      } catch (error) {
+        //handle error
+        console.error(error);
+      }
+    };
+
+    sendData();
+  }, [cart, cartUserID]);
+
   return (
     <Layout>
       {!isCartHidden && <Cart />}
---
+++ src/App/configs/firebase.js
@@ -0,0 +1,4 @@
+const FIREBASE_URL =
+  'https://react-http-be337-default-rtdb.firebaseio.com/cart/';
+
+export default FIREBASE_URL;
