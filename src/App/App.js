7a4d07aa18cdad08ad8dbe324bcd99164049704b
Marknjo <marknjo7@gmail.com>
2021-06-10 10:42:53 +0300

    Router: Setting up React router practical session

--- src/App/App.js
+++ src/App/App.js
@@ -1,35 +1,9 @@
-//import { consoleSeparator } from './helpers/consoleSeparator';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import MainHeader from './components/MainHeader';
-import ProductDetail from './pages/ProductDetail';
-import Products from './pages/Products';
-import Welcome from './pages/Welcome';
-
-const App = function () {
+function App() {
   return (
-    <>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/welcome" />
-          </Route>
+    <div>
 
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
-    </>
+    </div>
   );
-};
+}
 
 export default App;
