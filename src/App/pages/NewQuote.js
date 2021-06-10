a20e62ca4e2c1d5b16d4f947c6a2b9f41b9176e5
Marknjo <marknjo7@gmail.com>
2021-06-10 12:14:26 +0300

    Router: Showing add quote form component

--- src/App/pages/NewQuote.js
+++ src/App/pages/NewQuote.js
@@ -1,5 +1,11 @@
+import QuoteForm from '../components/quotes/QuoteForm';
+
 const NewQuote = function () {
-  return <h1>Add a New Quote page</h1>;
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData);
+  };
+
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
