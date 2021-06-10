78846cf301bedcd2800f32e9d9b8e37ca8faaa1b
Marknjo <marknjo7@gmail.com>
2021-06-10 14:41:53 +0300

    Router: Fetching all quotes from the firebase db

--- src/App/pages/Quotes.js
+++ src/App/pages/Quotes.js
@@ -1,4 +1,9 @@
+import { useEffect } from 'react';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
 
 const DUMMY_QUOTES = [
   { id: 'q1', author: 'Mark', text: 'Learning React is super fun!' },
@@ -6,7 +11,34 @@ const DUMMY_QUOTES = [
 ];
 
 const Quotes = function () {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === 'pending') {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
 export default Quotes;
