0632c5789ad985bb806490ef14f0ce66dc1b54ba
Marknjo <marknjo7@gmail.com>
2021-06-10 09:54:55 +0300

    Router: Router Link component

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,21 @@
 //import { consoleSeparator } from './helpers/consoleSeparator';
 import { Route } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
 import Products from './pages/Products';
 import Welcome from './pages/Welcome';
 
 const App = function () {
   return (
     <>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </>
   );
 };
