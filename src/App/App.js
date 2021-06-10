1f547adf3a48cecdaa17c927ec5f996bcb18ab3a
Marknjo <marknjo7@gmail.com>
2021-06-10 15:54:10 +0300

    Router: Lazy loading pages

--- src/App/App.js
+++ src/App/App.js
@@ -1,34 +1,46 @@
+import { Suspense } from 'react';
+import { lazy } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import Layout from './components/layout/Layout';
-import NewQuote from './pages/NewQuote';
-import NotFound from './pages/NotFound';
-import QuoteDetail from './pages/QuoteDetail';
-import Quotes from './pages/Quotes';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = lazy(() => import('./pages/NewQuote'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+const QuoteDetail = lazy(() => import('./pages/QuoteDetail'));
+const Quotes = lazy(() => import('./pages/Quotes'));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="quotes" />
-        </Route>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="quotes" />
+          </Route>
 
-        <Route path="/new-quote">
-          <NewQuote />
-        </Route>
+          <Route path="/new-quote">
+            <NewQuote />
+          </Route>
 
-        <Route path="/quotes" exact>
-          <Quotes />
-        </Route>
+          <Route path="/quotes" exact>
+            <Quotes />
+          </Route>
 
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail />
-        </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
 
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
