44c2d7289271d58b2ea23b228a30bffe547e4ef6
Marknjo <marknjo7@gmail.com>
2021-06-10 17:55:15 +0300

    End of react router lessons

--- src/App/App.js
+++ src/App/App.js
@@ -1,48 +1,11 @@
-import { Suspense } from 'react';
-import { lazy } from 'react';
-import { Redirect, Route, Switch } from 'react-router';
-import Layout from './components/layout/Layout';
-import LoadingSpinner from './components/UI/LoadingSpinner';
+//import { consoleSeparator } from './helpers/consoleSeparator';
 
-const NewQuote = lazy(() => import('./pages/NewQuote'));
-const NotFound = lazy(() => import('./pages/NotFound'));
-const QuoteDetail = lazy(() => import('./pages/QuoteDetail'));
-const Quotes = lazy(() => import('./pages/Quotes'));
-
-function App() {
+const App = function () {
   return (
-    <Layout>
-      <Suspense
-        fallback={
-          <div className="centered">
-            <LoadingSpinner />
-          </div>
-        }
-      >
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="quotes" />
-          </Route>
-
-          <Route path="/new-quote">
-            <NewQuote />
-          </Route>
-
-          <Route path="/quotes" exact>
-            <Quotes />
-          </Route>
-
-          <Route path="/quotes/:quoteId">
-            <QuoteDetail />
-          </Route>
-
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Suspense>
-    </Layout>
+    <div>
+      <h2>Start the next Session</h2>
+    </div>
   );
-}
+};
 
 export default App;
