8b7fb7331a6fa7dec7815503858851db1c912008
Marknjo <marknjo7@gmail.com>
2022-04-05 14:09:13 +0300

    V18#01:Routing Add a test Route

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
@@ -8,6 +9,8 @@ const appRoot = createRoot(document.getElementById('root')! as HTMLDivElement)
 
  appRoot.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
