d0cb7da7d7abeeb365d2c02da24f94205218ce7a
Marknjo <marknjo7@gmail.com>
2022-04-05 14:41:53 +0300

    Main: Add project starter code with Sample Router /Home and Layout Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
 import logo from './assets/logo.svg'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+/**
+ * 
+ * Sample Project Starter Code
+ * @TODO: Extract Home and Layout form the template
+ */
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
+
+
+/// Basic Home Route Component
+const Home = function() {
+  const [count, setCount] = useState(0)
+  return (
+      <Layout>
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
@@ -37,9 +56,21 @@ function App() {
             Vite Docs
           </a>
         </p>
-      </header>
-    </div>
+      </Layout>
   )
 }
 
-export default App
+// Sample
+const Layout:React.FC = function({children}) {
+  return (
+    <div className="App-header">
+      <header >
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+
+      <main>
+        {children}
+      </main>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 
 
 const appRoot = createRoot(document.getElementById('root')! as HTMLDivElement)
 
  appRoot.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
